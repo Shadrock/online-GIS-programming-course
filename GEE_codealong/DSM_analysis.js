ab8d438ca4cdd31d304abdd0953ae5622cc8de73
Shadrock Roberts <shadrock.roberts@gmail.com>
2020-10-18 23:11:04 -0400

    Update DSM_analysis.js

--- GEE_codealong/DSM_analysis.js
+++ GEE_codealong/DSM_analysis.js
@@ -1,6 +1,5 @@
 /*This code loads the dsm and shows the user specified display of 0 and 500 from the data. This code uses "ALOS DSM: global 30m" V2_2 in the Earth Engine catalogue. Inspect the bands to ensure the data set includes the AVE_DSM band. This analysis does not change the data set: it's just a visualization. */
 
-
 //manual way to searched for DSM, which I normally have them import using the search bar
 var image = ee.Image('JAXA/ALOS/AW3D30_V2_2');
 
@@ -27,4 +26,4 @@ var elevgt500 = elev.gt(500)
 elevgt500 = elevgt500.updateMask(elevgt500);
 Map.addLayer(elevgt500, {palette:['red']}, "elev>500m")
 
-// Zoom in a bit, use “layers” map control to turn off DSM, reduce opacity of analysis.
+// Zoom in a bit, use “layers” map control to turn off DSM, reduce opacity of the colors, and see how your analysis looks. 
